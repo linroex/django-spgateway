45343a9c6a698988e1eb0b318cf9919dd534ad2a
Superbil <superbil@gmail.com>
2020-02-05 05:24:37 +0800

    Update API version to 1.5

--- spgateway/forms.py
+++ spgateway/forms.py
@@ -6,5 +6,5 @@ class SpgatewayForm(forms.Form):
     MerchantID = forms.CharField(max_length=15, widget=forms.HiddenInput())
     TradeInfo = forms.CharField(widget=forms.HiddenInput())
     TradeSha = forms.CharField(widget=forms.HiddenInput())
-    Version = forms.CharField(max_length=5, initial='1.4', widget=forms.HiddenInput())
+    Version = forms.CharField(max_length=5, initial='1.5', widget=forms.HiddenInput())
     action = 'https://ccore.spgateway.com/MPG/mpg_gateway' if settings.DEBUG else 'https://core.spgateway.com/MPG/mpg_gateway'
