a6ccfb406cbb054b0b5338e0f9a9c86cbf52847a
Superbil <superbil@gmail.com>
2020-03-14 01:30:16 +0800

    Bump version to 1.0.0
    - update development status to beta
    - add natural language english

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = "0.4.0"
+__version__ = "1.0.0"
 
 with open('README.rst') as f:
     long_description = f.read()
@@ -16,13 +16,14 @@ setup(
     include_package_data = True,
     install_requires = ["django>=1.10", "pycrypto>=2.6.1"],
     classifiers = [
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Framework :: Django",
         "Framework :: Django :: 2.2",
         "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
         "Environment :: Web Environment",
     ],
     long_description = long_description,
